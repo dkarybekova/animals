6e03090ef9b8977a42804ed72c9003d402045a3a
dkarybekova <turganakmoldo@gmail.com>
2021-06-14 12:37:03 +0600

    Homework 24

--- app/src/main/java/kg/tutorialapp/animals/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/animals/MainActivity.kt
@@ -1,11 +1,35 @@
 package kg.tutorialapp.animals
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var image_1: ImageView
+    lateinit var image_2: ImageView
+    lateinit var image_3: ImageView
+    lateinit var textview: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textview = findViewById(R.id.textView) as TextView
+        image_1 = findViewById(R.id.image_1) as ImageView
+        image_1.setOnClickListener{
+            textview.text = getString((R.string.first))
+        }
+
+        image_2 = findViewById(R.id.image_2) as ImageView
+        image_2.setOnClickListener{
+            textview.text = getString((R.string.second))
+        }
+
+        image_3 = findViewById(R.id.image_3) as ImageView
+        image_3.setOnClickListener{
+            textview.text = getString((R.string.third))
+        }
     }
 }
