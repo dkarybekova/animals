ecac0d8e1b0a8c475585baecebbcb777a39b5835
dkarybekova <turganakmoldo@gmail.com>
2021-06-29 14:33:32 +0600

    Homework 42 Notification

--- app/src/main/java/kg/tutorialapp/animals/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/animals/MainActivity.kt
@@ -1,19 +1,28 @@
 package kg.tutorialapp.animals
 
+import android.content.Context
 import android.os.Bundle
 import android.content.Intent
+import android.os.Build
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var textview: TextView
     lateinit var btnHomeworks: Button
     lateinit var btnLessons: Button
+
+    private val NotificationID = 103
+    private val CHANNEL_ID = "channel_id_01"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,12 @@ class MainActivity : AppCompatActivity() {
 
         homeworkActivities()
         lessonActivities()
+        createNotificationsChannel()
+
+        val button = findViewById<Button>(R.id.btn_note)
+        button.setOnClickListener{
+            sendNotifications()
+        }
     }
 
     private fun homeworkActivities() {
@@ -57,4 +72,30 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
+
+    private fun createNotificationsChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val title = "Note Title"
+            val note_description = "Note Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, title, importance). apply {
+                description = note_description
+            }
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotifications() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_coronavirus_24)
+            .setContentTitle("Notification")
+            .setContentText("Ready")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(NotificationID, builder.build())
+        }
+    }
+
 }
